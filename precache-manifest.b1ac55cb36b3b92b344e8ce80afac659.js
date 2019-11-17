90505d24d22e465ec6c9365fb329ab1f3e3617f8
Anton Nakonechnyi <ext-anton.nakonechnyi@here.com>
2019-11-17 13:04:16 +0200

    Updates

---
+++ precache-manifest.b1ac55cb36b3b92b344e8ce80afac659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5beb97b6ea3c09ffe77ea3d0a5fd89",
+    "url": "/events-app/index.html"
+  },
+  {
+    "revision": "c13367f93169c9e2c021",
+    "url": "/events-app/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "e7870f85c3159557c526",
+    "url": "/events-app/static/css/main.57f89e81.chunk.css"
+  },
+  {
+    "revision": "c13367f93169c9e2c021",
+    "url": "/events-app/static/js/2.67229269.chunk.js"
+  },
+  {
+    "revision": "e7870f85c3159557c526",
+    "url": "/events-app/static/js/main.d1df163f.chunk.js"
+  },
+  {
+    "revision": "20cbfe7a093b92e29499",
+    "url": "/events-app/static/js/runtime-main.23f86df4.js"
+  }
+]);
